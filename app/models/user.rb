5ce1fa633f57645f65e888afcdfd5d92ce4476ae
Tran Duyet <duyettran0608@gmail.com>
2018-09-28 00:43:08 +0700

    Finish static pages

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :name,  presence: true, length: {maximum: Settings.users.max_name}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Settings.users.max_pass},
+    format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false}
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
