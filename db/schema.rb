20b7926f21775b859b452c9c926c1033250c0550
Tran Duyet <duyettran0608@gmail.com>
2018-10-02 13:16:36 +0700

    Modeling users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_09_27_183647) do
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,5 +18,4 @@ ActiveRecord::Schema.define(version: 2018_09_27_183647) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
