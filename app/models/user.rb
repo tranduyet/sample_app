d48e6d4d30e43b119ab13be5b726a2fe0bda9c4c
Tran Duyet <duyettran0608@gmail.com>
2018-10-03 20:53:38 +0700

    Basic-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: Settings.users.max_pass},
     format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false}
   has_secure_password
+  validates :password, presence: true, length: {minimum: Settings.users.min_pass}
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create string, cost: cost
+  end
 
   private
 
